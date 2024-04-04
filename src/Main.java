08acb1668e53263b12b8e95398f64b3a8c1eb689
Андрей <andrej.chugunov2002@yandex.ru>
2024-04-04 14:22:40 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Result №1 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
+         dog = dog + 4;
+         cat = cat + 4;
+         paper = paper + 4;
+        System.out.println("Result №2 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
+         dog = dog - 3.5;
+         cat = cat - 1.6;
+         paper = paper - 7639;
+        System.out.println("Result №3 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
+    }
+}
