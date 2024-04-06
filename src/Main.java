7f832360d342356ac49114ffabda312a2f963646
Андрей <andrej.chugunov2002@yandex.ru>
2024-04-06 17:16:34 +0300

    Save progress

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-System.out.println("123");
+System.out.println("Задние 1");
+byte a = -128;
+short b = -10000;
+int c = 2000000;
+long d = 900000000;
+float e = 1.11f;
+double f = 1.999999999;
+ System.out.println(" Значение переменной а с типом byte равно " + a + System.lineSeparator() +
+                 " Значение переменной b с типом short равно " + b + System.lineSeparator() +
+         " Значение переменной c с типом int равно " + c + System.lineSeparator() +
+         " Значение переменной d с типом long равно " + d + System.lineSeparator() +
+         " Значение переменной e с типом float равно " + e + System.lineSeparator() +
+         " Значение переменной f с типом double равно " + f);
     }
 }
