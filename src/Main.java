a9203958b45b83e16badd618b10e7869bb7f91a0
Андрей <andrej.chugunov2002@yandex.ru>
2024-04-06 14:49:02 +0300

    Empty

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-         System.out.println("Задача 1");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(" Result №1 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
-         System.out.println("Задача 2");
-          dog = dog + 4;
-          cat = cat + 4;
-          paper = paper + 4;
-        System.out.println(" Result №2 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
-         System.out.println("Задача 3");
-          dog = dog - 3.5;
-          cat = cat - 1.6;
-          paper = paper - 7639;
-        System.out.println(" Result №3 = " + "dog = " + dog + ", " + "cat = " + cat + ", " + "paper = " + paper + ".");
-         System.out.println("Задача 4");
-        var friend = 19;
-        System.out.println(" friend = " + friend);
-         friend = friend + 2;
-        System.out.println(" friend = " + friend);
-         friend = friend / 7;
-        System.out.println(" friend = " + friend);
-         System.out.println("Задача 5");
-        var frog = 3.5;
-        System.out.println(" frog = " + frog);
-          frog = frog * 10;
-        System.out.println(" frog = " + frog);
-          frog = frog / 3.5;
-        System.out.println(" frog = " + frog);
-          frog = frog + 4;
-        System.out.println(" frog = " + frog);
-        System.out.println("Задача 6");
+
     }
 }
